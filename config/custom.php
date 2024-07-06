bb14dba235cd0939951f98d2c1cff539fbe54101
sivanujan <smsivanujan@gmail.com>
2024-07-06 20:18:40 +0530

    Signed-off-by: sivanujan <smsivanujan@gmail.com>

--- config/custom.php
+++ config/custom.php
@@ -2,8 +2,6 @@
 
 return [
     'appscript_quote_excel' => env('APPSCRIPT_QUOTE_EXCEL', 'default_value'),
-    // 'appscript_visa_excel' => env('APPSCRIPT_VISA_EXCEL', 'default_value'),
-    // 'appscript_contact_excel' => env('APPSCRIPT_CONTACT_EXCEL', 'default_value'),
 ];
 
 
